d508c5d17fb3fb2b6c87983dade718116a358d82
jrf96324-cmd <jrf96324@gmail.com>
2026-05-09 10:17:46 +0800

    Build mobile PWA dashboard for center-stand durability tester

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'center-stand-pwa-v1';
+const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((response) => {
+          const cloned = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, cloned));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'));
+    })
+  );
+});
